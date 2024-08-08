d719a99c5f7fc78ee39a01898cca723db1e54340
Yosich1951 <61489721+Yosich1951@users.noreply.github.com>
2024-08-08 20:48:02 +0300

    add templates

--- dj02/main/views.py
+++ dj02/main/views.py
@@ -4,16 +4,9 @@ from django.http import HttpResponse
 # Create your views here.
 # На главную страницу
 def index(request):
-    return HttpResponse("<h1> Это мой первый проект на Django </>")
+    return render(request, 'main/index.html')
 
 # На другую страницу
 def new(request):
-    return HttpResponse("<h1> Это вторая страница моего проекта на Django </>")
+    return render(request, 'main/new.html')
 
-# На страницу data
-def data(request):
-    return HttpResponse("<h1> Это страница data моего проекта на Django </>")
-
-# На страницу test
-def test(request):
-    return HttpResponse("<h1> Это страница test моего проекта на Django </>")
